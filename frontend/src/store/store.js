7e09ee55f0996400f6a6f4480c750aea961f6f31
Jin <bblinging22@gmail.com>
2025-07-21 18:00:09 +0900

    Feature: initial setting

---
+++ frontend/src/store/store.js
@@ -0,0 +1,9 @@
+// store.js
+import { create } from 'zustand';
+
+const useStore = create((set) => ({
+    count: 0,
+    increase: () => set((state) => ({ count: state.count + 1 })),
+}));
+
+export default useStore;
