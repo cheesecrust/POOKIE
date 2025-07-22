920028c0261badf21999127f957f38934586f5c7
ParkJaeHwan <doormoo2@naver.com>
2025-07-22 17:35:18 +0900

    INTERNAL: ADD WS Dependency

---
+++ backend/src/main/java/com/ssafy/pookie/game/user/dto/UserDto.java
@@ -0,0 +1,13 @@
+package com.ssafy.pookie.game.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+    private String userId;
+    private String userNickname;
+}
