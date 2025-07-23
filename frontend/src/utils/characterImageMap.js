58b33b8059b286574d5cb802e924ca9c6e576af4
dayea <myannnng@gmail.com>
2025-07-23 16:37:12 +0900

    FEAT: atom_user

---
+++ frontend/src/utils/characterImageMap.js
@@ -0,0 +1,29 @@
+import pooding_strawberry from '../assets/character/pooding_strawberry.png'
+import pooding_blueberry from '../assets/character/pooding_blueberry.png'
+import pooding_caramel from '../assets/character/pooding_caramel.png'
+import pooding_choco from '../assets/character/pooding_choco.png'
+import pooding_creambrulee from '../assets/character/pooding_creambrulee.png'
+import pooding_matcha from '../assets/character/pooding_matcha.png'
+import pooding_melon from '../assets/character/pooding_melon.png'
+import pooding_milk from '../assets/character/pooding_milk.png'
+import pookie_green from '../assets/character/pookie_green.png'
+import pookie_red from '../assets/character/pookie_red.png'
+import pookie_yellow from '../assets/character/pookie_yellow.png'
+import pookiepookie from '../assets/character/pookiepookie.png'
+
+const characterImageMap = {
+    pooding_strawberry,
+    pooding_blueberry,
+    pooding_caramel,
+    pooding_choco,
+    pooding_creambrulee,
+    pooding_matcha,
+    pooding_melon,
+    pooding_milk,
+    pookie_green,
+    pookie_red,
+    pookie_yellow,
+    pookiepookie,
+}
+
+export default characterImageMap;
