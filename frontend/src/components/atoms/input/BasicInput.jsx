d5d914ff101504a0284d1534119302c948f3bc22
Jin <bblinging22@gmail.com>
2025-07-24 19:21:47 +0900

    Feature: feature atoms_input

---
+++ frontend/src/components/atoms/input/BasicInput.jsx
@@ -0,0 +1,31 @@
+// src/components/atoms/input/BasicInput.jsx
+// 사용 예시:
+// useState import 필수
+// 아래 함수 선언 필수
+// const handleKeyDown = (e) => {
+//   if (e.key === "Enter") {
+//     console.log("엔터 입력됨!", text);
+//     setText("");
+//   }
+// }
+// <BasicInput
+//   value={text}
+//   onChange={(e) => setText(e.target.value)}
+//   onKeyDown={handleKeyDown}
+//   placeholder="내용을 입력하세요"
+// />
+
+const BasicInput = ({ value, onChange, onKeyDown, placeholder }) => {
+    return (
+        <input
+            type="text"
+            value={value}
+            onChange={onChange}
+            onKeyDown={onKeyDown}
+            placeholder={placeholder}
+            className="w-[350px] h-[50px] rounded border-2 border-black shadow-md outline-none px-2 text-lg text-black bg-white"
+        />
+    )
+}
+
+export default BasicInput
