64ab7780f8192101fb180ff859baff0ab956c7f1
dayea <myannnng@gmail.com>
2025-07-24 13:20:55 +0900

    Feat/변수명 변경

--- frontend/src/components/atoms/message/MessageCard.jsx
+++ frontend/src/components/atoms/message/MessageCard.jsx
@@ -1,24 +1,24 @@
 // 경로: src/components/atoms/message/MessageCard.jsx
 // 사용 예시
-//   <MessageCard type="received" nickname="다예" date="2023-08-05 12:00:00" message="안녕하세용" isRead={false} onDelete={() => {}} onReport={() => {}} />
+//   <MessageCard messageType="received" nickname="다예" date="2023-08-05 12:00:00" message="안녕하세용" isRead={false} onDelete={() => {}} onReport={() => {}} />
 
-const MessageCard = ({type, nickname,date, message,isRead,onDelete,onReport}) => {
+const MessageCard = ({messageType, nickname,date, messageContent,isRead,onDelete,onReport}) => {
     return (
         <div className="relative w-full p-4 rounded-md bg-white border border-gray-300 shadow-sm">
           <div className="flex justify-between items-center mb-2 text-sm text-gray-600">
-            <span>{type === 'sent' ? `받은사람 : ${nickname}` : `보낸 사람 : ${nickname}`} </span>
+            <span>{messageType === 'sent' ? `받은사람 : ${nickname}` : `보낸 사람 : ${nickname}`} </span>
             <span>{date}</span>
           </div>
     
 
-          <div className="text-base text-gray-800 mb-6">{message}</div>
+          <div className="text-base text-gray-800 mb-6">{messageContent}</div>
     
           <div className="absolute bottom-2 right-2 flex gap-2 text-xs">
-            {type === 'sent' && (
+            {messageType === 'sent' && (
               <span className="text-gray-500 italic">{isRead ? '읽음' : ''}</span>
             )}
     
-            {type === 'received' && (
+            {messageType === 'received' && (
               <button onClick={onReport} className="text-red-500 hover:underline">신고</button>
             )}
     
