38489a23c4a7bb2db3d7eb84fe64aa7d5b8b1a38
cheesecrust <jack0810@naver.com>
2025-07-25 12:45:21 +0900

    Feature: game data entity and query

--- backend/src/main/java/com/ssafy/pookie/auth/repository/UserAccountsRepository.java
+++ backend/src/main/java/com/ssafy/pookie/auth/repository/UserAccountsRepository.java
@@ -2,14 +2,16 @@ package com.ssafy.pookie.auth.repository;
 
 import com.ssafy.pookie.auth.model.UserAccounts;
 import com.ssafy.pookie.auth.model.base.Users;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserAccountsRepository extends JpaRepository<UserAccounts, Integer> {
-    Optional<UserAccounts> findById(Long id);
+public interface UserAccountsRepository extends JpaRepository<UserAccounts, Long> {
+
+    @NotNull Optional<UserAccounts> findById(@NotNull Long id);
 
     boolean existsByNickname(String nickname);
 
