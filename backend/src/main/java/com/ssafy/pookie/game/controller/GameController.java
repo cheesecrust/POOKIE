b2a4008d35c646f43a40ad0c83a611273cf9d687
Hwannee00 <doormoo2@naver.com>
2025-08-15 00:23:02 +0900

    FIX: Safe prepareStart

--- backend/src/main/java/com/ssafy/pookie/game/controller/GameController.java
+++ backend/src/main/java/com/ssafy/pookie/game/controller/GameController.java
@@ -1,12 +1,15 @@
 package com.ssafy.pookie.game.controller;
 
 import com.ssafy.pookie.game.server.manager.OnlinePlayerManager;
+import com.ssafy.pookie.game.user.dto.LobbyUserDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.socket.CloseStatus;
+
+import java.io.IOException;
 
 @Slf4j
 @RestController
@@ -19,7 +22,9 @@ public class GameController {
     @GetMapping("/init")
     public String init() {
         try {
-            onlinePlayerManager.getLobby().values().stream().forEach((user) -> user.getUser().getSession().close(CloseStatus.POLICY_VIOLATION));
+            for (LobbyUserDto user : onlinePlayerManager.getLobby().values()) {
+                user.getUser().getSession().close(CloseStatus.POLICY_VIOLATION);
+            }
             onlinePlayerManager.getRooms().clear();
             onlinePlayerManager.getLobby().clear();
         } catch (Exception e) {
